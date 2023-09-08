d318f3aeb357becd834620fda917684bd3029c63
Piotr Koska <piotr.s.koska@gmail.com>
2023-09-08 21:21:07 +0200

    Update add URNs

--- digitalocean/digitalocean_droplet/outputs.tf
+++ digitalocean/digitalocean_droplet/outputs.tf
@@ -3,4 +3,10 @@ output "droplet_ip_addresses" {
     for droplet in digitalocean_droplet.student_hosts:
     droplet.name => droplet.ipv4_address
   }
+  description = "value of the droplet ip addresses"
+}
+
+output "droplet_urns" {
+  value = [for droplet in digitalocean_droplet.student_hosts : droplet.urn]
+  description = "URNs of the droplets"
 }
