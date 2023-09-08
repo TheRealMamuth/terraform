cd6d1883b2cb0c67bce95106d10bd997b06c949a
Piotr Koska <piotr.s.koska@gmail.com>
2023-09-08 22:14:05 +0200

    update

--- digitalocean/digitalocean_droplet/main.tf
+++ digitalocean/digitalocean_droplet/main.tf
@@ -14,6 +14,6 @@ data "digitalocean_project" "playground" {
 resource "digitalocean_project_resources" "barfoo" {
   project = data.digitalocean_project.playground.id
   resources = [
-    flatten(digitalocean_droplet.student_hosts[*].id)
+    toset(digitalocean_droplet.student_hosts[*].id)
   ]
 }
