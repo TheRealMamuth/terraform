321a3d13a49c35b553b74d6a420a49a5ee8ba84b
Piotr Koska <piotr.s.koska@gmail.com>
2023-09-08 21:04:26 +0200

    Update

--- digitalocean/digitalocean_droplet/main.tf
+++ digitalocean/digitalocean_droplet/main.tf
@@ -1,8 +1,8 @@
 resource "digitalocean_droplet" "student_hosts" {
     count = var.student_count
-    image = var.droplet_image
-    name = "${var.droplet_name}-${count.index}"
-    region = var.droplet_region
-    size = var.droplet_size
-    vpc_uuid = var.droplet_vpc_uuid
+    image = var.droplet_config["image"]
+    name = "${var.droplet_config["name"]}-${count.index}"
+    region = var.droplet_config["region"]
+    size = var.droplet_config["size"]
+    vpc_uuid = var.droplet_config["vpc_uuid"]
 }
--- digitalocean/digitalocean_droplet/vars.tf
+++ digitalocean/digitalocean_droplet/vars.tf
@@ -1,37 +1,13 @@
-variable "droplet_name" {
-    description = "This is name for your droplet"
-    default     = "default"
-    type        = string
-}
-
-variable "droplet_image" {
-    description = "This is image for your droplet"
-    default     = "ubuntu-20-04-x64"
-    type        = string
-}
-
-variable "droplet_region" {
-    description = "This is region for your droplet"
-    default     = "fra1"
-    type        = string
-}
-
-variable "droplet_size" {
-    description = "This is size for your droplet"
-    default     = "s-1vcpu-1gb"
-    type        = string
-}
-  
-variable "droplet_agent" {
-    description = "This is agent for your droplet"
-    default     = "true"
-    type        = string
-}
-
-variable "droplet_vpc_uuid" {
-    description = "This is vpc uuid for your droplet"
-    default     = ""
-    type        = string
+variable "droplet_config" {
+  type = map(any)
+  default = {
+    name     = "default",
+    image    = "ubuntu-20-04-x64",
+    region   = "fra1",
+    size     = "s-1vcpu-1gb",
+    agent    = "true",
+    vpc_uuid = ""
+  }
 }
 
 variable "student_count" {
