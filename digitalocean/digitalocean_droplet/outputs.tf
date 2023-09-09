591edd9f295233e1406b027486cbaaa0e2d4006d
Piotr Koska <piotr.s.koska@gmail.com>
2023-09-09 15:10:52 +0200

    Add output in digitalocean

--- digitalocean/digitalocean_droplet/outputs.tf
+++ digitalocean/digitalocean_droplet/outputs.tf
@@ -10,3 +10,15 @@ output "droplet_urns" {
   value       = [for droplet in digitalocean_droplet.student_hosts : droplet.urn]
   description = "URNs of the droplets"
 }
+
+output "droplet_map" {
+  value = [
+    for droplet in digital_droplet.student_hosts : {
+      name = droplet.name
+      id = droplet.id
+      ip = droplet.ipv4_address
+      ip_priv = droplet.ipv4_address_private
+    }
+  ]
+  
+}
